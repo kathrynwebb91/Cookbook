ede97acd1d54ff6cf21d556b60bf3d7c66da0d59
Matthew Livingston <matt.l.livingston@gmail.com>
2011-11-25 12:56:57 +0000

    src files

---
+++ src/com/cookbook/CookbookActivity.java
@@ -0,0 +1,27 @@
+package com.cookbook;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.Toast;
+
+public class CookbookActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Toast.makeText(CookbookActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
